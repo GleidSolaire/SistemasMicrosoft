79a82573edda22b7f2ff7f1c24781a5c6df3b13e
Gleidson Almeida <gleids.solaire@yandex.com>
2019-08-10 17:49:40 -0300

    Classes
    Classes implementadas

---
+++ SistemaAereo/FormaPagamento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAereo
+{
+    class FormaPagamento
+    {
+        string Numcartao;
+        string codvalidador;
+        string bandeira;
+        DateTime datavalidade;
+        String formaoperacao;
+    }
+}
---
+++ SistemaAereo/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAereo
+{
+    class Pessoa
+    {
+        String nome;
+        String sobrenome;
+        String documento;
+        DateTime nascimento;
+        String telefone;
+        int id;
+    }
+}
