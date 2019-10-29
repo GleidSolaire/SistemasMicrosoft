cca242b2d928c7ec1943bc02a2952a75b7e5aace
GleidSolaire <gleids.solaire@yandex.com>
2019-10-28 21:08:51 -0300

    Add exercicio rotas

---
+++ ExercicioRotas/ExercicioRotas/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ExercicioRotas.Models;
+
+namespace ExercicioRotas.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About(string Id)
+        {
+            ViewData["Message"] = "Your ID is:"+Id;
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ExercicioRotas/ExercicioRotas/Controllers/OPController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExercicioRotas.Controllers
+{
+    public class OPController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Dobrar(int Id)
+        {
+            ViewData["Resultado"] = "->" + Id * 2;
+
+            return View();
+        }
+        
+        //public IActionResult Somar (int Id, int parametrob)
+        //{
+        //    ViewData["Resultado"] =  + Id + parametrob;
+
+        //    return View();
+        //}
+        public IActionResult Somar(int a, int b, int c)
+        {
+            ViewData["Resultado"] = a + b + c;
+
+            return View();
+        }
+
+        public IActionResult Subtrair(int Id, int parametrob)
+        {
+            ViewData["Resultado"] = +Id - parametrob;
+
+            return View();
+        }
+       
+
+    }
+}
